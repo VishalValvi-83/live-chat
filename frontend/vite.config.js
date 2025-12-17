95a08051c787c5e140d884297bb96ae8d68c94e3
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-12-17 15:39:30 +0530

    create basic ui with templates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
 })
