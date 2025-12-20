60b6344184b00ad1a206948c231be1c0f3324808
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-12-20 11:08:53 +0530

    fix: toastcontainer bug fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import PrivacySettingsPage from './pages/PrivacySettingsPage'
 import AppearanceSettingsPage from './pages/AppearanceSettingsPage'
 import PreLoader from './components/PreLoader'
 import './index.css'
-
+import { ToastContainer } from 'react-toastify';
 export const PreloaderContext = createContext({ preloaderComplete: false })
 export const usePreloader = () => useContext(PreloaderContext)
 
@@ -31,6 +31,18 @@ function App() {
   return (
     <ThemeProvider>
       <PreloaderContext.Provider value={{ preloaderComplete }}>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
         {showPreLoader && <PreLoader onComplete={handlePreloaderComplete} />}
         <Router>
           <Routes>
