55f424dc9f141393500058ba856dceca203c573b
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-12-30 14:48:01 +0530

    feat: add rate limiting middleware for API and authentication routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,18 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import authRoutes from "./routes/auth/authRoutes.js";
 import messageRoutes from './routes/messageRoute.js'
 import chatRoutes from "./routes/chatRoutes.js"
 import userRoutes from './routes/userRoutes.js'
 import "./config/mysql.js";
 import "./config/mongo.js";
-import dotenv, { config } from "dotenv";
-
+import dotenv from "dotenv";
+import { apiLimiter, authLimiter } from "./middleware/rateLimiter.js";
 
 const app = express();
-dotenv.config(app)
+dotenv.config();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 
 
@@ -21,7 +20,9 @@ app.get("/", (req, res) => {
   res.send("Live Chat Backend Running...");
 });
 
-app.use("/api/auth", authRoutes);
+app.use("/api/", apiLimiter);
+
+app.use("/api/auth", authLimiter, authRoutes);
 app.use("/api/messages", messageRoutes)
 app.use("/api/chat", chatRoutes)
 app.use("/api/user", userRoutes)
